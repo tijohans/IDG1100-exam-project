51035217c06873f9da24722650e52017ec2816c6
Thor Ivar Nirisen Johansen <tijohans@stud.ntnu.no>
2021-12-09 20:36:56 +0100

    Auto Push 09-12-21-20:36

--- gitupdate.sh
+++ gitupdate.sh
@@ -3,6 +3,6 @@ PUSHTIME=`date +"%d-%m-%y-%R"`
 
 git add .
 
-git commit -m "Auto Push ${date}"
+git commit -m "Auto Push ${PUSHTIME}"
 
 git push https://raspi:zfcEv46KsuRAhLCTmkKG@gitlab.stud.iie.ntnu.no/tijohans/idg1100-exam-project.git
